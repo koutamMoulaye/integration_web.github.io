a0a96d06c42060663395ce9d60ff40b8bc3ad738
waranomed <108696638+waranomed@users.noreply.github.com>
2022-07-11 00:25:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,191 @@
-function wcqib_refresh_quantity_increments() {
-    jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
-        var c = jQuery(b);
-        c.addClass("buttons_added"), c.children().first().before('<input type="image" src="images/icon-minus.svg" value="-" class="minus" >'), c.children().last().after('<input type="image" src="images/icon-plus.svg" value="+" class="plus">')
-    })
-}
-String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-    var a = this,
-        b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-    return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-}), jQuery(document).ready(function() {
-    wcqib_refresh_quantity_increments()
-}), jQuery(document).on("updated_wc_div", function() {
-    wcqib_refresh_quantity_increments()
-}), jQuery(document).on("click", ".plus, .minus", function() {
-    
-    var a = jQuery(this).closest(".quantity").find(".qty"),
-        b = parseFloat(a.val()),
-        c = parseFloat(a.attr("max")),
-        d = parseFloat(a.attr("min")),
-        e = a.attr("step");
-    b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+
+// images slide
+$(document).ready(function() {
+  var $firstSlide = $('.carousel li').first(),
+    $lastSlide = $('.carousel li').last(),
+    $slideWidth = $('.carousel-container').width(),
+    $numOfSlides = $('.carousel li').length;
+  $('.carousel-container').after('<ul class="thumbnails"></ul>');
+
+  $('.carousel li').each(function(index) {
+    $this = $(this);
+    $this.addClass('slide-' + index);
+    $this.clone().appendTo('.thumbnails');
+  });
+
+  function slideResize() {
+    $slideWidth = $('.carousel-container').width();
+    $('.carousel li').css('width', $slideWidth);
+    $('.arrow').css('height', '100%');
+  }
+  
+  $('.thumbnails li').first().addClass('active');
+  slideResize();
+
+  $firstSlide.addClass('active');
+  $lastSlide.insertBefore($firstSlide);
+
+  $('.carousel').css('margin-left', '-100%');
+
+  $(window).on('resize', slideResize);
+  
+  function nextSlide(interval, iterations) {
+    var howMany = 0;
+
+    function goNext() {
+      $firstSlide = $('.carousel li').first();
+      $lastSlide = $('.carousel li').last();
+      
+      slideResize();
+      $('.arrow').hide();
+      $firstSlide.animate({
+        width: "0"
+      }, interval, function() {
+
+        $firstSlide.insertAfter($lastSlide);
+        $firstSlide.css('width', $slideWidth);
+
+        $('.carousel li.active').next('li').addClass('active');
+        $('.carousel li.active').first().removeClass('active');
+
+        if ($('.thumbnails li').last().hasClass('active')) {
+          $('.thumbnails li').removeClass('active');
+          $('.thumbnails li').first().addClass('active');
+        } else {
+          $('.thumbnails li.active').next('li').addClass('active');
+          $('.thumbnails li.active').first().removeClass('active');
+        }
+        howMany++;
+        if (howMany < iterations) {
+          goNext();
+        } else {
+        $('.arrow').show();
+        }
+      });
+    }
+    goNext();
+  }
+
+  function prevSlide(interval, iterations) {
+    var howMany = 0;
+
+    function goPrev() {
+      $firstSlide = $('.carousel li').first();
+      $lastSlide = $('.carousel li').last();
+      
+      slideResize();
+      $('.arrow').hide();
+      var $prevSlide = $('.active').prev();
+      $lastSlide.css('width', '0');
+      $lastSlide.insertBefore($firstSlide);
+      $lastSlide.animate({
+        width: $slideWidth
+      }, interval, function() {
+        $('.carousel li.active').prev('li').addClass('active');
+        $('.carousel li.active').last().removeClass('active');
+
+        if ($('.thumbnails li').first().hasClass('active')) {
+          $('.thumbnails li').removeClass('active');
+          $('.thumbnails li').last().addClass('active');
+        } else {
+          $('.thumbnails li.active').prev('li').addClass('active');
+          $('.thumbnails li.active').last().removeClass('active');
+        }
+        howMany++;
+        if (howMany < iterations) {
+          goPrev();
+        } else {
+        $('.arrow').show();
+        }
+      });
+    }
+    goPrev();
+  }
+    $('.carousel-container').on('swipeleft', function() {
+      if ($('li:animated').length === 0) {
+    nextSlide(1000);
+      } else {
+        return;
+      }
+  });
+
+  $('.carousel-container').on('swiperight', function() {
+    if ($('li:animated').length === 0) {
+    prevSlide(1000);
+    } else {
+      return;
+    }
+  });
+  
+  $('.right-arrow').on('click', function() {
+    nextSlide(1000);
+  });
+
+  $('.left-arrow').on('click', function() {
+    prevSlide(1000);
+  });
+
+  $('body').on('click', '.thumbnails li', function() {
+
+    var $this = $(this),
+      $desiredSlideIndex = $this.index(),
+      $currentSlideIndex = $('ul.thumbnails li.active').index(),
+      slideDifference = $desiredSlideIndex - $currentSlideIndex;
+
+    console.log('desired slide: ' + $desiredSlideIndex);
+    console.log('current slide: ' + $currentSlideIndex);
+    console.log('slide difference: ' + slideDifference);
+    if (slideDifference > 0) {
+      nextSlide(500, slideDifference);
+    } else if (slideDifference < 0) {
+      slideDifference = slideDifference * -1;  
+      prevSlide(500, slideDifference);
+    } else {
+      return;
+    }
+  });
+
 });
+// ///////////////////////////PARTIE REPONSIVE///////////////////////////
 
-let count = 0 ;
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
 
-const counter = document.getElementById('counter')
-document.getElementById('add-animation','plus').addEventListener('click' , event =>{
-    const cl = counter.classList
-    const c = 'span';
-    count++;
-
-    counter.innerText = count;
-    cl.remove(c, cl.countains(c));
-    setTimeout(() => 
-    counter.classList.add('span'))
-
-
+toggle.addEventListener('click', function() {
+    body.classList.toggle('open');
 })
 
+// ////////////////////image carousel responsive////////////////////
+let img__slider= document.getElementsByClassName('img__slider');
+console.log(img__slider);
+
+let etape = 0;
+
+let nbr__img = img__slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+
+function enleverActiveImages() {
+  for(let i = 0 ; i < nbr__img ; i++){
+    img__slider[i].classList.remove('active');
+  } 
+}
+
+suivant;addEventListener('click', function(){
+  etape++;
+  if(etape>= nbr__img){
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add('active');
+})
+setInterval(function(){
+  etape++;
+  if(etape>= nbr__img){
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add('active');
+}, 5000)
